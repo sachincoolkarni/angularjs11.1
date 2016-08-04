73f38c3f68539fe7e89b781a139fbff61a41431a
sachincoolkarni <sachincoolkarni@users.noreply.github.com>
2016-08-04 12:06:25 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var mod=angular.module("myapp",[]);
+mod.controller("mycontroller",["$scope",function($scope)
+
+{
+	$scope.name="sachin";
+	$scope.empid="TOS124";
+	$scope.desg="Software Engineer";
+}]);
